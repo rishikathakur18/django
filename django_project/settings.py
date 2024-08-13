872167f9416faf61f86c50cffc823d0b25493195
rishikathakur18 <rishikathakur2211@gmail.com>
2024-08-13 12:03:04 +0530

    read_env changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-from dotenv import read_dotenv
-read_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
